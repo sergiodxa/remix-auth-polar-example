8eceda890d53ed6d84aa30e7a074254d17b47605
Sergio Xalambrí <hello@sergiodxa.com>
2025-03-05 01:26:36 -0500

    Refactor callback component to return null for error boundary handling

--- app/routes/callback.tsx
+++ app/routes/callback.tsx
@@ -1,16 +1,12 @@
 import { redirect } from "react-router";
 import { authenticator } from "~/auth";
-import type { Route } from "./+types/home";
+import type { Route } from "./+types/callback";
 import { sessionStorage } from "~/session";
 
-export default function Component({ loaderData }: Route.ComponentProps) {
-  return (
-    <pre>
-      <code style={{ tabSize: 2 }}>
-        {JSON.stringify(loaderData, null, "\t")}
-      </code>
-    </pre>
-  );
+export default function Component() {
+  // This component is never rendered, but it's needed so in case of error we
+  // can show an error boundary.
+  return null;
 }
 
 export async function loader({ request }: Route.ActionArgs) {
